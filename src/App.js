f350f1dfe5a7a58f93623ffcc54b9d7a8e0144b7
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-23 16:05:54 -0500

    fixed DRY code and added Character.js file

--- src/App.js
+++ src/App.js
@@ -12,39 +12,17 @@ const App = () => {
   // Fetch API data
 
   useEffect(() => {
-    const fetchCharacters = async () => {
-      const response = await axios(`https://swapi.dev/api/people/?page=1`)
-      const speciesResponse = await axios('https://swapi.dev/api/species/?page=1')
-      console.log(response.data.results);
-
-      for (const character of response.data.results) {
-        const homeworld = await axios.get("https" + character.homeworld.slice(4));
-        character.homeworld = homeworld.data.name;
-                                                                          
-        if (character.species.length !== 0) {
-          const species = await axios.get("https" + character.species[0].slice(4));
-          !species.data.name ? character.species = "Human" : character.species = species.data.name;
-        } else {
-          character.species = "Human"
-        }
-      }
-
-      setCharacters(response.data.results);
-      setIsLoading(false);
-    }
-
-    fetchCharacters();
+    fetchCharacters(`https://swapi.dev/api/people/?page=1`)
   }, [])
-
-  // Search Function
 
   const characterSearch = async (searchTerm) => {
     setIsLoading(true);
-    const characterSearchResponse = await axios.get(
-      `https://swapi.dev/api/people/?search=${searchTerm}`
-    )
+    fetchCharacters(`https://swapi.dev/api/people/?search=${searchTerm}`)
+  };
 
-    for (const character of characterSearchResponse.data.results) {
+  const fetchCharacters = async (url) => {
+      const response = await axios(url)
+      for (const character of response.data.results) {
       const homeworld = await axios.get("https" + character.homeworld.slice(4));
       character.homeworld = homeworld.data.name;
 
@@ -56,9 +34,9 @@ const App = () => {
       }
     }
 
-    setCharacters(characterSearchResponse.data.results);
+    setCharacters(response.data.results);
     setIsLoading(false);
-  };
+  }
 
   return (
     <div>
